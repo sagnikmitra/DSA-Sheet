739b728842116e37682537d116f462a14707f6f9
Sagnik Mitra <sagnikmitra123@gmail.com>
2021-08-27 23:09:33 +0530

    reversing a linkedlist

---
+++ LinkedList/reverse-a-linkedlist.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int data){
+        this->data = data;
+        next = NULL;
+    }
+};
+struct LinkedList{
+    Node* head;
+    LinkedList() { head = NULL; }
+    void reverse(){
+        Node* current = head;
+        Node* prev = NULL; 
+        Node* next = NULL;
+        while(current != NULL){
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    void print(){
+        struct Node* temp = head;
+        while(temp != NULL){
+            cout<<temp->data <<" ";
+            temp = temp->next;
+        }
+    }
+
+    void push(int data){
+        Node* temp = new Node(data);
+        temp->next = head;
+        head = temp;
+    }
+};
+
+int main(){
+    LinkedList ll;
+    ll.push(20);
+    ll.push(4);
+    ll.push(2);
+    ll.push(1);
+    ll.push(5);
+
+    cout << "Given Linked List \n";
+    ll.print();
+
+    ll.reverse();
+
+    cout<<"Reversed LinkedList \n";
+    ll.print();
+
+    return 0;
+}
